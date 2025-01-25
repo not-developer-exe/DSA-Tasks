949410daec28d91888b08748feb9e240a1eb6343
not-developer-exe <official.additya.singh@gmail.com>
2025-01-25 12:14:13 +0530

    Map questions added

---
+++ Practice1/index.js
@@ -0,0 +1,41 @@
+///// Basics of JS done
+
+/// MAP practice 
+
+// let arr = [1,2,3,4]
+
+// let ans = arr.map(val=>val*2)
+// console.log(ans);
+
+//// Capitalize strings
+
+// let str = ['hello', 'world', 'hi', 'kaise', 'ho']
+
+// let ans = str.map(elem=> elem.charAt(0).toUpperCase() + elem.slice(1))
+// console.log(ans);
+
+///// Extract Name from an array of obj
+
+// let arr = [{ name: "Alice", age: 25 }, { name: "Bob", age: 30 }]
+
+// const ans = arr.map(elem=>elem.name)
+
+// console.log(ans);
+
+//// Nested Map
+
+// let arr = [[1,2], [3,4]]
+
+// const ans = arr.map(elem=>elem.map(elem=>elem*elem))
+
+// console.log(ans);
+
+////Currency conversion 
+
+// let INR = [10, 20, 30]
+
+// let USD = INR.map(inr=>inr*82)
+
+// console.log(USD);
+
+
