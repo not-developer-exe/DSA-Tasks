ea8dfd81cb357a507c357c21a7c073b0529911bb
not-developer-exe <official.additya.singh@gmail.com>
2025-02-07 11:45:43 +0530

    Some new questions added including insertion Sort

---
+++ Random/index.js
@@ -0,0 +1,64 @@
+/// Prime Number
+
+let n = 5
+// let count = 0;
+
+// for(let i = 0; i<=n; i++){
+//     if(n%i==0) count++
+// }
+// console.log(count == 2 ? `${n} is prime` : 'not prime');
+
+///// Fibo
+
+// const fibo = (n)=>{
+//         if(n==0 || n==1) return n
+    
+//         return fibo(n-1) + fibo(n-2)
+// }
+    
+// const fiboSeries = (n) => {
+//         for (let i = 0; i < n; i++) {
+//             console.log(fibo(i));
+    
+//         }
+// };
+    
+// fiboSeries(n)
+
+
+// const fibo2 = (n) => {
+//     let a = 0, b = 1;
+//     for (let i = 0; i < n; i++) {
+//         console.log(a);
+//         [a, b] = [b, a + b]; 
+//     }
+// };
+
+// fibo2(5);
+
+
+
+
+//////////  Insertion Sort
+
+// let arr = [7, 4, 3, 5, 1, 9];
+
+// for (let i = 1; i < arr.length; i++) {
+//     let key = arr[i];
+//     let j = i - 1;
+
+//     while (j >= 0 && arr[j] > key) {
+//         arr[j + 1] = arr[j];
+//         j--;
+//     }
+//     arr[j + 1] = key;
+// }
+
+// console.log(arr);
+
+// How It Works (Step-by-Step)
+// ---Start with the second element (because a single element is already "sorted").
+// ---Pick the element and compare it with the elements before it (from right to left).
+// ---Shift larger elements to the right to make space.
+// ---Place the picked element in the correct position.
+// ---Repeat this process for all elements.
--- Strings/Class1/index.js
+++ Strings/Class1/index.js
@@ -436,5 +436,27 @@
     
 // }
 
+//// Sieve of Eratosthenes 
+
+// let n = 1000
+
+// const checkPrime = () =>{
+//     let arr = new Array(n+1).fill(true)
+//     for(let i = 2; i<=Math.floor(Math.sqrt(n)); i++){
+//         if (arr[i] == true){
+//             for(let j = i*i; j<=n; j+=i){
+//                 arr[j] = false
+//             } 
+//         }
+//     }
+
+//     for(let i = 2; i<arr.length; i++){
+//         if(arr[i]==true) console.log(i);
+        
+//     }
+
+// }
+
+// checkPrime(n)
 
 
